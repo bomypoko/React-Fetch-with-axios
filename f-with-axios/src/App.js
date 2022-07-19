2cf18914e2b2cbaf07dba24099b64fe189422bc3
Ratchasri Kidking <orc_hero@hotmail.com>
2022-07-19 15:49:44 +0700

    Build Input box

--- f-with-axios/src/App.js
+++ f-with-axios/src/App.js
@@ -4,19 +4,21 @@ import axios from 'axios'
 function App() {
 
   const [data , setData ] = useState({hits: []})
+  const [query , setQuery] = useState('react')
+
   useEffect(() => {
     const fetchData = async() => {
-      const results = await axios('http://hn.algolia.com/api/v1/search?query=react')
+      const results = await axios(`http://hn.algolia.com/api/v1/search?query=${query}`)
       setData(results.data)
       console.log(results.data)
     }
 
     fetchData()
-  }, [] )
+  }, [query] )
 
   return (
     <>
-
+        <input type='text' value={query} onChange={event => setQuery(event.target.value)}></input>  
             <ul>
               {data.hits.map(item => (
                 <li key={item.objectID}> 
